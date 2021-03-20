6a9a06dfdbb55eb6eb3769a0d371566d1b3445b0
nitin kashyap <76529658+nitinkashyap1821@users.noreply.github.com>
2021-03-20 12:23:48 +0530

    Update geolocator_service.dart

--- lib/services/geolocator_service.dart
+++ lib/services/geolocator_service.dart
@@ -6,11 +6,11 @@ class GeolocatorService {
 
   Stream<Position> getCurrentLocation(){
 
-    return Geolocator.getPositionStream(desiredAccuracy: LocationAccuracy.bestForNavigation,forceAndroidLocationManager: true,intervalDuration: Duration(seconds: 5));
+    return Geolocator.getPositionStream(desiredAccuracy: LocationAccuracy.bestForNavigation,forceAndroidLocationManager: false,intervalDuration: Duration(seconds: 5));
   }
 
-  Future<Position> getInitialLocation() async {
-    Position position= await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high,forceAndroidLocationManager: true);
+  Future<Position> getInitialLocation()async{
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high,forceAndroidLocationManager: false);
     return position;
   }
 
