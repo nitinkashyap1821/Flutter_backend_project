fe6d1409e2cc3f6b4d07db4f8a09dd65f25e0810
nitin kashyap <76529658+nitinkashyap1821@users.noreply.github.com>
2021-03-21 20:58:46 +0530

    Update jsonFile.dart

--- lib/jsonFiles/jsonFile.dart
+++ lib/jsonFiles/jsonFile.dart
@@ -4,23 +4,24 @@ import 'package:path_provider/path_provider.dart';
 
 
 class Json{
-  
 
-  save(String key1,dynamic value1,String key2, dynamic value2) async {
+  File jsonFile;
+  Directory dir;
+  String fileName = "myJSONFile.json";
+
+  writeToFile(dynamic value1, dynamic value2) async {
     final directory = await getApplicationDocumentsDirectory();
     print("path = "+directory.path);
-    final file = File('${directory.path}/myfile.json');
-    Map<String,dynamic> content1 = {key1: value1};
-    Map<String,dynamic> content2 = {key2: value2};
-    await file.writeAsString(jsonEncode(content1),mode: FileMode.append);
-    await file.writeAsString(jsonEncode(content2),mode: FileMode.append);
+    Map<String,dynamic> content1 = {'LAT': value1};
+    Map<String,dynamic> content2 = {'LNG': value2};
+    final jsonFile = File('${directory.path}/$fileName');
+    if(jsonFile != null){
+      Map<String, dynamic> jsonFileContent = json.decode(jsonFile.readAsStringSync());
+      jsonFileContent.addAll(content1);
+      jsonFileContent.addAll(content2);
+      await jsonFile.writeAsString(jsonEncode(content1));
+      await jsonFile.writeAsString(jsonEncode(content2));
+    }
   }
-
-
-
-
-
-
-
 }
 
