79142c8516912b644a81bb71d741a1a308642b85
nitin kashyap <76529658+nitinkashyap1821@users.noreply.github.com>
2021-03-22 19:01:38 +0530

    Update geolocator_service.dart

--- lib/services/geolocator_service.dart
+++ lib/services/geolocator_service.dart
@@ -1,9 +1,7 @@
 import 'package:geolocator/geolocator.dart';
-import 'package:x/jsonFiles/jsonFile.dart';
 
 class GeolocatorService {
 
-  final Json json = Json();
 
   Stream<Position> getCurrentLocation(){
     return Geolocator.getPositionStream(desiredAccuracy: LocationAccuracy.bestForNavigation,forceAndroidLocationManager: false,intervalDuration: Duration(seconds: 5));
